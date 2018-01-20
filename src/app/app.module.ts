92674a78a2f7bbc2b0f5e599851dc4f40d961f42
daiyy2009 <daiyy2009@hotmail.com>
2018-01-21 00:43:50 +0800

    完成angular CLI和material集成。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FocusMonitor } from "@angular/cdk/a11y";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { MyComponent } from "./my-component/my-component";
+import { SliderOverviewExample } from "./slider-overview/slider-overview-example";
+import { MatSliderModule, MatTableModule } from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MyComponent, SliderOverviewExample],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatSliderModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
